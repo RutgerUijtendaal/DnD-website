0f3a3be38a093630c1b2429ef5d8f03b9577626b
Rutger Uijtendaal <rutger.uijtendaal@gmail.com>
2021-08-10 18:50:25 +0200

    anon pictures

--- src/app/views/about-view/about-view.component.ts
+++ src/app/views/about-view/about-view.component.ts
@@ -12,7 +12,7 @@ export class AboutViewComponent implements OnInit {
     {
       name: 'Briian',
       function: 'Captain',
-      imagePath: 'assets/img/team/briian.png',
+      imagePath: 'assets/img/team/briian_anon.png',
       content: 'The Bosman \n\n Quiet, contemplating and with a voice that\'ll make you want to follow. \n\nHis insults leave you feeling refreshed.'
     },
     {
@@ -30,13 +30,13 @@ export class AboutViewComponent implements OnInit {
     {
       name: 'JD',
       function: 'Treasurer, Gun Operator',
-      imagePath: 'assets/img/team/jd.png',
+      imagePath: 'assets/img/team/jd_anon.png',
       content: 'Sneaky, stealthy... and he\'s already got your wallet. \n\nGood with coins, better with a knife and gun.'
     },
     {
       name: 'Dr Steele',
       function: 'Science Officer',
-      imagePath: 'assets/img/team/caliel.png',
+      imagePath: 'assets/img/team/caliel_anon.png',
       content: 'Ex-corpo, alcoholic, genius? \n\nAlways has a bottle nearby, always knows how to fix your computer.'
     },
   ]
